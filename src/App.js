6bda3359711f1650f5ce8b78d27d7298ce6d4979
Chinmay <chinz.sandur07@gmail.com>
2024-02-26 15:49:00 +0530

    add card component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import NewsItem from './components/NewsItem';
 
 export default class App extends Component {
 
@@ -11,7 +12,22 @@ export default class App extends Component {
     return (
       <div>
         <NavBar />
-        <News/>
+        <h2>NewsMonkey Headlines</h2>
+
+        <div className="row">
+          <div className="column col-md-4">
+            <NewsItem title="myTitle" desc="myDesc" url="https://s11279.pcdn.co/wp-content/uploads/2024/02/Screenshot-2024-02-09-134618-630x393.png" />
+          </div>
+          <div className="column col-md-4">
+            <NewsItem title="myTitle" desc="myDesc" url="" />
+          </div>
+          <div className="column col-md-4">
+            <NewsItem title="myTitle" desc="myDesc" url="" />
+          </div>
+
+
+
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 export class NewsItem extends Component {
     render() {
+        const { title, desc, url } = this.props;
         return (
-            <div>
-                This is news Item Component
+            <div className='container my-3'>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={url} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>
+                            {desc}
+                        </Card.Text>
+                        <Button variant="primary btn-sm">Read More</Button>
+                    </Card.Body>
+                </Card>
+
             </div>
         )
     }
