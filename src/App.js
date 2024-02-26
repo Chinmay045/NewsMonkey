2d95adfadb48cedf8629601a28d3a7fe562ff823
Chinmay <chinz.sandur07@gmail.com>
2024-02-26 13:20:58 +0530

    add navbar and news components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
-  c = "John"
+
   render() {
     return (
       <div>
-        Hello {this.c}
+        <NavBar />
+        <News/>
       </div>
     )
   }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+export class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar expand="lg" className="bg-body-tertiary">
+                    <Container>
+                        <Navbar.Brand href="#home">NewsMonkey</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#link">Link</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        )
+    }
+}
+
+export default NavBar
