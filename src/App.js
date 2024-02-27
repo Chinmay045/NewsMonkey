620a2834e6c6a8a75f48f59e7ec1d193a54823a9
Chinmay <chinz.sandur07@gmail.com>
2024-02-27 18:44:12 +0530

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import NavBar from './components/NavBar';
 // import News from './components/News';
 // import NewsItem from './components/NewsItem';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default class App extends Component {
 
